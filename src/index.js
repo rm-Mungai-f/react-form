83d0b99f2c79de4e8338b0f0fb224967e6e12b9d
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-24 13:10:16 +0300

    implement useState hook inproject

--- src/index.js
+++ src/index.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 const LoginForm=()=>{
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    const handleEmail=(event)=>{
+        setEmail(event.target.value);
+    }
+
+    const handlePassword=(event)=>{
+        setPassword(event.target.value);
+    }
+
     const handleSubmit=(event)=>{
         event.preventDefault();
+
+        alert(email+' '+password);
     }
+
     return(
         <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="email">Email</label>
-                <input id="email" type="text"/>
+                <input id="email" type="text" value={email} onChange={handleEmail} />
             </div>
             <div>
                 <label htmlFor="password">Password</label>
-                <input id="password" type="password"/>
+                <input id="password" type="password" value={password} onChange={handlePassword} />
             </div>
             <button type="submit">Submit</button>
         </form>
